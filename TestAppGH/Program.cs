80883182646859b3ef09b3f36c6476ad1cf0ee26
vidovici <80092432+vidovici@users.noreply.github.com>
2021-03-07 14:11:57 +0100

    Add project files.

---
+++ TestAppGH/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppGH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suha krpa na dnu mora! :-)");
+            Console.ReadLine();
+        }
+    }
+}
